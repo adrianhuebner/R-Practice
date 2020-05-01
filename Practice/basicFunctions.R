e6c9ef26f8e28ffdb033414cada6988d5b5c73d4
adrianhueber <adrianh92@gmail.com>
2020-04-30 22:23:01 -0700

    notes added and more basic functions written

--- Practice/basicFunctions.R
+++ Practice/basicFunctions.R
@@ -32,4 +32,27 @@
           paste("xyz", c(2,5,7, "test", 4.5))
        # If you don't want there to be seperation between the charcters you can use the argument sep
           paste("xyz", 1:10, sep = "")
-  # rep (replicate) 
+  # rep (replicate) replicates the value of x
+       # Again the first time running this function run the following command to learn about it:
+          ?rep
+       # This function has a very simple structure, you feed it x and then the number of times you want it repeated, the desired length of the output, or each element of x is repeated each times
+       # Here is a simple way to write this function:
+          rep(c(3,4,5), 3)
+       # You can also write it by writing out arguments such as the following:
+          rep(1:10, times = 3)
+       # x can also be created outside of the rep function which can be written like the following:
+          x = c(1,2,3)
+          rep(x, each = 3)
+       # multiple arguments can be used such as adding each and times together
+          rep(x, each = 3, times = 3)
+
+# How to identify and work with index positions in R
+  # Let's say that the following vector (numbers 4-20) has been provided:
+        x = 4:20
+          # The whole thing has a length of 17, important to remember that index position starts at 1
+  # To see how many index positions have the value of 10 which would be written as the following:
+        which(x == 10)
+          # The answer will be: [1]7 which tells you that there is only one position that has a value of 10 and that index is 7
+  # To see how to find the value of a certain index, that is written as the following:
+        x[3]
+          # The answer will tell you what the value of the position is
